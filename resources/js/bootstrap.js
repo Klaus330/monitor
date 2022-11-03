d39b33a1dca3def0197ba9d239fa3b239f671833
Claudiu Popa <claudiup340@gmail.com>
2022-11-03 10:28:26 +0200

    Fix vite issue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,18 @@
-window._ = require('lodash');
+import lodash from "lodash";
+import axios from "axios";
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+window._ = lodash
 
-window.axios = require('axios');
+
+// window._ = require('lodash');
+
+// /**
+//  * We'll load the axios HTTP library which allows us to easily issue requests
+//  * to our Laravel back-end. This library automatically handles sending the
+//  * CSRF token as a header based on the value of the "XSRF" token cookie.
+//  */
+
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
